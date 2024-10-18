7a3c9529c04945478b4b1553b751d115fab8d89c
karanDevX11 <bistakaran89@gmail.com>
2024-10-18 09:13:33 +0545

    Add coffee machine file

--- intermediate_python_projects/importingpackage.py
+++
@@ -1,9 +0,0 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-table.field_names = ["Province", "District", "City"]
-table.add_rows([
-    ["Koshi", "Sunsari", "Itahari"],
-    ["Bagmati", "Kathmandu", "Kathmandu"],
-])
-table.align = "l"
-print(table)
